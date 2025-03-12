8671fc73ddebe16b04d8b81acba75835c51b657c
aryan-gupta-jiit <aryangupta.jiit@gmail.com>
2025-03-13 00:15:35 +0530

    footer,newsletter,policy

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Hero from '../components/Hero'
+import OurPolicy from '../components/OurPolicy'
+import NewsletterBox from '../components/NewsletterBox'
+
+const Home = () => {
+  return (
+    <div>
+      <Hero/>
+      <OurPolicy/>
+      <NewsletterBox/>
+    </div>
+  )
+}
+
+export default Home
