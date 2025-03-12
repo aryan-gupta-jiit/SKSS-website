0fa602e88479f55f85d3f6d802ecfb7847d163fc
aryan-gupta-jiit <aryangupta.jiit@gmail.com>
2025-03-13 01:30:49 +0530

    collections

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar'
 import Home from './pages/Home'
 import { Routes,Route } from 'react-router-dom'
 import Footer from './components/Footer'
+import Collection from './pages/Collection'
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
     <Navbar/>
     <Routes>
       <Route path="/" element={<Home/>}/>
+      <Route path="/collection" element={<Collection/>}/>
     </Routes>
     <Footer/>
     </div>
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,11 +2,15 @@ import React from 'react'
 import Hero from '../components/Hero'
 import OurPolicy from '../components/OurPolicy'
 import NewsletterBox from '../components/NewsletterBox'
+import LatestCollection from '../components/LatestCollection'
+import BestSeller from '../components/BestSeller'
 
 const Home = () => {
   return (
     <div>
       <Hero/>
+      <LatestCollection/>
+      <BestSeller/>
       <OurPolicy/>
       <NewsletterBox/>
     </div>
