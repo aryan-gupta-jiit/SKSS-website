6d61dd29f48e48a63636ffbf0e5147cea6018a6a
aryan-gupta-jiit <aryangupta.jiit@gmail.com>
2025-04-04 12:38:46 +0530

    orderspage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,6 +10,7 @@ import SearchBar from './components/SearchBar'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import PlaceOrder from './pages/PlaceOrder'
+import Orders from './pages/Orders'
 
 const App = () => {
   return (
@@ -23,6 +24,7 @@ const App = () => {
       <Route path='/product/:productId' element={<Product/>}/>
       <Route path='/cart' element={<Cart/>}/>
       <Route path='/place-order' element={<PlaceOrder/>}/>
+      <Route path='/orders' element={<Orders/>}/>
     </Routes>
     <Footer/>
     </div>
